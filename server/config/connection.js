1a197ed1e36d44fb2877af61ca51004d81cf39cc
Trevor Pino <trevorpino@yahoo.com>
2021-04-14 16:45:24 -0600

    basic site layout

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/private-reserve', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/flippin-finds', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
