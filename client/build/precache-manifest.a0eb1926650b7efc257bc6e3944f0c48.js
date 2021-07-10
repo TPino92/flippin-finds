9b226f93415ed68999452dc126d9d7d259c5381c
Trevor Pino <trevorpino@yahoo.com>
2021-07-09 19:08:52 -0600

    npm audit fixes

---
+++ client/build/precache-manifest.a0eb1926650b7efc257bc6e3944f0c48.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05f1b047e2f2a0f020e363da8484681a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef4d7ea4903a6f44220d",
+    "url": "/static/css/main.91155e50.chunk.css"
+  },
+  {
+    "revision": "48104055eb29cc31ffbc",
+    "url": "/static/js/2.2cd69236.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.2cd69236.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef4d7ea4903a6f44220d",
+    "url": "/static/js/main.5e053b9d.chunk.js"
+  },
+  {
+    "revision": "8d1c34504318064c4883",
+    "url": "/static/js/runtime-main.c2921892.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
